77f074758f34b8c77375c491eaa63b6f24a723e9
Wilfried OLLIVIER <wollivier@fdn.fr>
2019-12-03 14:05:05 +0100

    Add solution for day 3

---
+++ 3/3.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+# impoooooorts
+import sys
+
+# Check args
+if len(sys.argv) != 2:
+    print("Santa if not happy, some of the arguments are missing")
+    sys.exit(1)
+
+# Open and read file
+with open(sys.argv[1]) as f:
+    # get the cable w and x paths
+    w, x = f.read().splitlines()
+
+
+# Format data inside arrays
+w = w.split(",")
+x = x.split(",")
+
+# Translate a move to a computable direction
+delta = {"R": (0, 1), "L": (0, -1), "U": (1, 1), "D": (1, -1)}
+
+"""
+compute both position and steps for a point in the grid
+"""
+
+
+def compute_points(move, pos, steps, path):
+    # split move code, first part is direction, second part is distance
+    direc, dist = move[0], int(move[1:])
+    # translate this direction to real usable values using delta directory
+    index, val = delta[direc]
+
+    # loop over distance
+    for _ in range(dist):
+        # compute position
+        pos[index] += val
+        # add step
+        steps += 1
+        # add a new point at position pos inside path
+        path[tuple(pos)] = steps
+
+    # return steps in order to keep to reused it
+    return steps
+
+
+"""
+compute both position and steps for a wire (or a set of points)
+"""
+
+
+def compute_wire(w):
+    # position in the grid, updated as we run the wire
+    pos = [0, 0]
+    # dict containing point position and associated steps to reach it
+    path = {}
+    # steps counter for current wire
+    steps = 0
+    # for each move in the wire
+    for move in w:
+        # compute point, add it to path, return updated steps
+        steps = compute_points(move, pos, steps, path)
+
+    # once we reach the end of the wire, return directory containing wire data
+    return path
+
+
+# compute wire data for each wire
+wp = compute_wire(w)
+xp = compute_wire(x)
+
+# Find intersections between wires
+intersect = set(wp.keys()) & set(xp.keys())
+
+# minimal distance, juste use points position
+res = min(abs(a)+abs(b) for (a, b) in intersect)
+
+print("Result (1) : %i" % res)
+
+# path distance, use steps instead of positions
+res = min(abs(wp[ints])+abs(xp[ints]) for ints in intersect)
+
+print("Result (2) : %i" % res)
---
+++ 3/README.md
@@ -0,0 +1,39 @@
+# Day 3: Crossed Wires
+
+## Subject
+
+For example, if the first wire's path is R8,U5,L5,D3, then starting from the central port (o), it goes right 8, up 5, left 5, and finally down 3:
+
+    ...........
+    ...........
+    ...........
+    ....+----+.
+    ....|....|.
+    ....|....|.
+    ....|....|.
+    .........|.
+    .o-------+.
+    ...........
+
+Then, if the second wire's path is U7,R6,D4,L4, it goes up 7, right 6, down 4, and left 4:
+
+    ...........
+    .+-----+...
+    .|.....|...
+    .|..+--X-+.
+    .|..|..|.|.
+    .|.-X--+.|.
+    .|..|....|.
+    .|.......|.
+    .o-------+.
+    ...........
+
+These wires cross at two locations (marked X), but the lower-left one is closer to the central port: its distance is 3 + 3 = 6.
+
+## Solution
+
+Language used : [Python](https://www.python.org/)
+
+### Run
+
+    python 3.py
