4fca3af6b07f3656dee56e3f231d27a78a897c47
Wilfried OLLIVIER <wollivier@fdn.fr>
2019-12-08 22:29:25 +0100

    Add solution for day 8, part 2

--- 8/index.ts
+++ 8/index.ts
@@ -26,6 +26,7 @@ try {
     layers.push(image.slice(index, index + lsize));
   }
 
+  // Part 1
   // create a counter object that handle all the information for next steps
   let counterz = layers.map(l => {
     return {
@@ -41,7 +42,57 @@ try {
   });
 
   // compute result
-  console.log(counterz[0].o * counterz[0].t);
+  console.log("Result, part 1 : " + counterz[0].o * counterz[0].t);
+
+  // Part 2
+  // use first layer as base
+  let base = layers[0];
+
+  // Copute final layer
+  let final = base
+    // find up color element in layers
+    .map((e, i) => {
+      // if the element is `transparent` go into deeper layers
+      if (e == 2) {
+        // Go into other layers to find current value (start at 1 since base array is init with layer 0)
+        for (let index = 1; index < layers.length; index++) {
+          // if a color is found, return the value
+          if (layers[index][i] != 2) {
+            // apply layer color value
+            return layers[index][i];
+          }
+        }
+        // Should not reach this part
+        return undefined;
+      }
+      // if the element of the first layer is a color, return it
+      return e;
+    })
+    // just for fun
+    .map(e => {
+      if (e == 0) {
+        // empty
+        return "⬛";
+      } else {
+        // oh oh oh
+        return "🎅";
+      }
+    });
+
+  // final image
+  let img = [];
+  // some index
+  let idx = 0;
+
+  // build image
+  while (idx < final.length) {
+    // push part of the image to final image slice
+    img.push(final.slice(idx, idx + w));
+    idx += w;
+  }
+
+  // nice print
+  console.log("Result, part 2\n" + img.join("\n"));
 } catch (e) {
   console.error("Error:", e.stack);
   process.exit(1);
