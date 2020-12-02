0dd885b353d6c5cbe358003b08f9dcf40f0cbf99
Wilfried OLLIVIER <wollivier@fdn.fr>
2020-12-02 11:43:59 +0100

    Add tests for day 2

--- 2k20/aoc/test/aoc_test.exs
+++ 2k20/aoc/test/aoc_test.exs
@@ -6,4 +6,9 @@ defmodule AOCTest do
     assert AOC.D1.run1() == 996_996
     assert AOC.D1.run2() == 9_210_402
   end
+
+  test "Day 2" do
+    assert AOC.D2.run1() == 393
+    assert AOC.D2.run2() == 690
+  end
 end
