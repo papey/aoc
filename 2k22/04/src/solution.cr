098b9f8ebc90f1232c74b21ca918cf2becb53b47
Wilfried OLLIVIER <wollivier@fdn.fr>
2022-12-04 14:48:23 +0100

    Add day 04

---
+++ 2k22/04/src/solution.cr
@@ -0,0 +1,29 @@
+require "../../helpers/input"
+
+module Day04
+  def self.part1
+    input = Input.new("../input/in")
+
+    input.split(cleanup: true).count do |sections|
+      a_low, a_high, b_low, b_high = parse(sections)
+      a_low <= b_low && b_high <= a_high || b_low <= a_low && a_high <= b_high
+    end
+  end
+
+  def self.part2
+    input = Input.new("../input/in")
+
+    input.split(cleanup: true).count do |sections|
+      a_low, a_high, b_low, b_high = parse(sections)
+      a_low <= b_low <= a_high || b_low <= a_low <= b_high
+    end
+  end
+
+  def self.parse(line)
+    /(\d+)-(\d+),(\d+)-(\d+)/.match(line).try &.captures.compact_map { |bound| bound.try &.to_i } || [] of Int32
+  end
+end
+
+puts "Part 1 : #{Day04.part1}"
+
+puts "Part 2 : #{Day04.part2}"
