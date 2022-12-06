98edc05917357c040c80d9cd765eea3e80bfba5b
Wilfried OLLIVIER <wollivier@fdn.fr>
2022-12-06 08:34:34 +0100

    Add day 06

---
+++ 2k22/06/src/solution.cr
@@ -0,0 +1,26 @@
+require "../../helpers/input"
+
+module Day06
+  PKT_SIZE =  4
+  MSG_SIZE = 14
+
+  def self.part1
+    input = Input.new("../input/in")
+
+    solve(input.split.first.each_char, PKT_SIZE)
+  end
+
+  def self.part2
+    input = Input.new("../input/in")
+
+    solve(input.split.first.each_char, MSG_SIZE)
+  end
+
+  def self.solve(dataframe, size)
+    dataframe.cons(size).index { |part| part.uniq.size == size }.not_nil! + size
+  end
+end
+
+puts "Part 1 : #{Day06.part1}"
+
+puts "Part 2 : #{Day06.part2}"
