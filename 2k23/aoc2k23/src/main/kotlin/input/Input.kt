a459aed0b72c5bd5e6e9d81c9937937066badb47
Wilfried OLLIVIER <wollivier@fdn.fr>
2023-12-05 13:23:44 +0100

    Chore: refactor input read method

--- 2k23/aoc2k23/src/main/kotlin/input/Input.kt
+++ 2k23/aoc2k23/src/main/kotlin/input/Input.kt
@@ -3,11 +3,8 @@ package input
 import java.io.FileNotFoundException
 
 fun read(name: String, trim: Boolean = true): List<String> {
-    val classLoader = object {}.javaClass.classLoader
-    val inputStream = classLoader.getResourceAsStream(name) ?: throw FileNotFoundException()
-
-    return inputStream.bufferedReader().use { it.readText() }
-        .split("\n")
+    return raw(name)
+       .split("\n")
         .map { if (trim) it.trim() else it }
         .filter { it.isNotBlank() }
 }
