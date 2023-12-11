5eac3c39a228eb28784c2faa80dc181f1610539d
Wilfried OLLIVIER <wollivier@fdn.fr>
2023-12-11 08:46:04 +0100

    Add day 10 part 2

--- 2k23/aoc2k23/src/main/kotlin/10.kt
+++ 2k23/aoc2k23/src/main/kotlin/10.kt
@@ -5,7 +5,7 @@ import java.util.*
 
 fun main() {
     println("Part 1: ${part1(read("10.txt"))}")
-//    println("Part 2: ${part2(read("10.txt"))}")
+    println("Part 2: ~${part2(read("10.txt"))}, you need to debug a bit by yourself ¯\\_(ツ)_/¯")
 }
 
 fun part1(input: List<String>): Int {
@@ -16,7 +16,7 @@ fun part1(input: List<String>): Int {
 
     val queue: Queue<List<Point>> = Direction.entries.filter { dir ->
         val adj = maze.adjacents(start.to(dir))
-        adj.any { it.x == start.x && it.y == start.y }
+        adj.any { it == start }
     }.map {
         listOf(start.to(it), start)
     }.let {
@@ -33,7 +33,7 @@ fun part1(input: List<String>): Int {
         maze.setDistance(point, maze.getDistance(prev) + 1)
 
         maze.adjacents(point).forEach {
-            if (it.x != prev.x || it.y != prev.y) {
+            if (it != prev) {
                 queue.add(listOf(it, point))
             }
         }
@@ -41,7 +41,44 @@ fun part1(input: List<String>): Int {
 }
 
 fun part2(input: List<String>): Int {
-    return 0
+    val maze = Maze(input)
+
+    val start = maze.getStart()!!
+    maze.setDistance(start, 0)
+    maze.setInside(start, 'S')
+
+    val stack: Stack<List<Point>> = Direction.entries.filter { dir ->
+        if (!maze.isValid(start.to(dir))) {
+            false
+        } else {
+            val adj = maze.adjacents(start.to(dir))
+            adj.any { it == start }
+        }
+    }.map {
+        listOf(start.to(it), start)
+    }.let {
+        val s: Stack<List<Point>> = Stack()
+        s.addAll(it)
+        s
+    }
+
+    while (true) {
+        val (point, prev) = stack.pop()
+
+        if (point == start) {
+            maze.enclosed()
+            maze.printInside()
+            return maze.countInside()
+        }
+
+        maze.discover(point)
+
+        maze.adjacents(point).forEach {
+            if (it != prev) {
+                stack.push(listOf(it, point))
+            }
+        }
+    }
 }
 
 class Point(val x: Int, val y: Int) {
@@ -53,6 +90,19 @@ class Point(val x: Int, val y: Int) {
             Direction.EAST -> Point(x + 1, y)
         }
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Point) return false
+
+        return x == other.x && y == other.y
+    }
+
+    override fun hashCode(): Int {
+        var result = x
+        result = 31 * result + y
+        return result
+    }
 }
 
 class Maze(input: List<String>) {
@@ -60,6 +110,7 @@ class Maze(input: List<String>) {
     private val w: Int = input.first().toCharArray().size
     val map: List<CharArray> = input.map { it.toCharArray() }
     private val distances: List<MutableList<Int>> = List(map.size) { MutableList(map[0].size) { -1 } }
+    private val inside: List<MutableList<Char>> = List(map.size) { MutableList(map[0].size) { ' ' } }
 
     fun isValid(point: Point): Boolean = point.x in 0..<w && point.y in 0..<h
 
@@ -75,15 +126,75 @@ class Maze(input: List<String>) {
         return null
     }
 
+    fun printInside() {
+        (0..<h).forEach { y ->
+            (0..<w).forEach { x ->
+                print(getInside(Point(x, y)))
+            }
+            println()
+        }
+    }
+
+    fun getValue(point: Point): Char = map[point.y][point.x]
+
     fun setDistance(point: Point, dist: Int) {
         distances[point.y][point.x] = dist
     }
 
     fun getDistance(point: Point): Int = distances[point.y][point.x]
 
+    private fun getInside(point: Point): Char = inside[point.y][point.x]
+
+    fun setInside(point: Point, value: Char) {
+        inside[point.y][point.x] = value
+    }
+
     fun adjacents(point: Point): List<Point> {
-        return SYMBOL_TO_DIRECTION[map[point.y][point.x]]!!.filter { dir -> isValid(point.to(dir)) }
+        return SYMBOL_TO_DIRECTION[getValue(point)]!!.filter { dir -> isValid(point.to(dir)) }
             .map { point.to(it) }
+    }
+
+    fun discover(point: Point) {
+        setInside(point, getValue(point))
+    }
+
+    fun enclosed() {
+        forEachPosition {
+            val boundaries = (it.x + 1..<w).count { x ->
+                val boundary = getInside(Point(x, it.y))
+                listOf('|', 'J', 'L').contains(boundary)
+            }
+
+            if (getInside(it) == ' ') {
+                setInside(
+                    it, if (boundaries % 2 == 0) {
+                        'O'
+                    } else {
+                        'I'
+                    }
+                )
+            }
+        }
+    }
+
+    fun countInside(): Int {
+        var count = 0
+
+        forEachPosition {
+            if (getInside(it) == 'I') {
+                count++
+            }
+        }
+
+        return count
+    }
+
+    private fun forEachPosition(operation: (Point) -> Unit) {
+        (0..<h).forEach { y ->
+            (0..<w).forEach { x ->
+                operation(Point(x, y))
+            }
+        }
     }
 }
 
---
+++ 2k23/aoc2k23/src/main/kotlin/11.kt
@@ -0,0 +1,16 @@
+package d11
+
+import input.read
+
+fun main() {
+    println("Part 1: ${part1(read("11.txt"))}")
+    println("Part 2: ${part2(read("11.txt"))}")
+}
+
+fun part1(input: List<String>): Int {
+    return 0
+}
+
+fun part2(input: List<String>): Int {
+    return 0
+}
