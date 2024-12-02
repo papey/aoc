b87c676e06a192f9307d15bd1812977d9a1e1bde
Wilfried OLLIVIER <wollivier@fdn.fr>
2024-12-02 13:15:02 +0100

    Add day 02

---
+++ 2k24/aoc/lib/d2.ex
@@ -0,0 +1,27 @@
+defmodule D2 do
+  def p1 do
+    Parser.parse("inputs/d2.txt")
+    |> then(&Parser.into_integer_values/1)
+    |> Enum.filter(&safe?(&1))
+    |> Enum.count()
+  end
+
+  def p2 do
+    Parser.parse("inputs/d2.txt")
+    |> then(&Parser.into_integer_values/1)
+    |> Enum.filter(&safe?(&1, :tolerance))
+    |> Enum.count()
+  end
+
+  defp safe?(l) do
+    differences = l |> Enum.chunk_every(2, 1, :discard) |> Enum.map(fn [a, b] -> a - b end)
+
+    Enum.all?(differences, &(&1 in 1..3)) || Enum.all?(differences, &(&1 in -1..-3//-1))
+  end
+
+  defp safe?(l, :tolerance) do
+    0..(length(l) - 1)
+    |> Enum.map(&List.delete_at(l, &1))
+    |> Enum.any?(&safe?/1)
+  end
+end
--- 2k24/aoc/lib/parser.ex
+++ 2k24/aoc/lib/parser.ex
@@ -4,4 +4,10 @@ defmodule Parser do
     |> String.split("\n", trim: true)
     |> Enum.map(&String.trim/1)
   end
+
+  def into_integer_values(inputs) do
+    inputs
+    |> Enum.map(&String.split(&1, "\s"))
+    |> Enum.map(fn values -> Enum.map(values, &String.to_integer/1) end)
+  end
 end
