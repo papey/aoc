25674c8ace3867c425085293f133f212bb930086
Wilfried OLLIVIER <wollivier@fdn.fr>
2024-12-24 08:03:54 +0100

    Add day 23

--- 2k24/aoc/lib/d23.rb
+++ 2k24/aoc/lib/d23.rb
@@ -8,36 +8,43 @@ def parse
 end
 
 def p1
-  conns = parse.each_with_object(Hash.new { |h, k| h[k] = [] }) do |(a, b), h|
-    h[a] << b
-    h[b] << a
-  end
+conns =
+    parse.each_with_object(Hash.new { |h, k| h[k] = [] }) do |(a, b), h|
+      h[a] << b
+      h[b] << a
+    end
 
-  triplets(conns).count { |computers| computers.any? { |c| c.start_with?("t") } }
+  triplets(conns).count do |computers|
+    computers.any? { |c| c.start_with?("t") }
+  end
 end
 
 def p2
-  conns = parse.each_with_object(Hash.new { |h, k| h[k] = [] }) do |(a, b), h|
-    h[a] << b
-    h[b] << a
-  end
+  conns =
+    parse.each_with_object(Hash.new { |h, k| h[k] = [] }) do |(a, b), h|
+      h[a] << b
+      h[b] << a
+    end
 
   bron_kerbosch([], conns.keys, [], conns).max_by(&:size).sort.join(",")
 end
 
 def triplets(conns)
-  conns.each_with_object([Set.new, Set.new]) do |(origin, connected), (triplets, visited)|
-    connected.each do |n1|
-      next if visited.include?([origin, n1].sort)
-      visited << [origin, n1].sort
-      conns[n1].each do |n2|
+  conns
+    .each_with_object(
+      [Set.new, Set.new]
+    ) do |(origin, connected), (triplets, visited)|
+      connected.each do |n1|
+        next if visited.include?([origin, n1].sort)
+        visited << [origin, n1].sort
+        conns[n1].each do |n2|
+          next if [origin, n1, n2].uniq.size < 3 || !connected.include?(n2)
 
-        next if [origin, n1, n2].uniq.size < 3 || !connected.include?(n2)
-
-        triplets << [origin, n1, n2].sort
+          triplets << [origin, n1, n2].sort
+        end
       end
     end
-  end.first
+    .first
 end
 
 def bron_kerbosch(r, p, x, conns, cliques = [])
